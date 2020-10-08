fc49600b6a3944287b164788a35825344c8d45d2
Dejan <dejan.sheki.lukic@gmail.com>
2020-10-08 20:15:33 +0200

    Start

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Healthy Food</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+</body>
+
+</html>
